3e34e55f8d2b6f18ecb2f7912ef1159755886d8f
Aedulapuram Manivyas <62017824+Manivyas@users.noreply.github.com>
2023-06-20 22:29:09 +0530

    Create aws-config.js

---
+++ be/aws-config.js
@@ -0,0 +1,8 @@
+const AWS = require('aws-sdk');
+AWS.config.update({
+  accessKeyId: 'AKIASZOTK3WSQZQFSCGQ',
+  secretAccessKey: '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',
+  region: 'ap-south-1',
+});
+
+module.exports = AWS;
