823d9f25db3178303ad5c2c6e4f0682af25144f9
Aedulapuram Manivyas <62017824+Manivyas@users.noreply.github.com>
2023-06-20 22:31:53 +0530

    Update aws-config.js

--- be/aws-config.js
+++ be/aws-config.js
@@ -1,7 +1,7 @@
 const AWS = require('aws-sdk');
 AWS.config.update({
-  accessKeyId: 'AKIASZOTK3WSQZQFSCGQ',
-  secretAccessKey: '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',
+  accessKeyId: '/',
+  secretAccessKey: '/',
   region: 'ap-south-1',
 });
 
