d4632097c1933e9a29c5798671166b006d578a73
Aedulapuram Manivyas <62017824+Manivyas@users.noreply.github.com>
2023-06-20 22:26:09 +0530

    Update TestingPdf.tsx

--- fe/src/components/TestingPdf.tsx
+++ fe/src/components/TestingPdf.tsx
@@ -1,38 +1,43 @@
 import { Viewer, Worker } from '@react-pdf-viewer/core';
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
-// using ES6 modules
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-
-import '@react-pdf-viewer/core/lib/styles/index.css';
-import '@react-pdf-viewer/default-layout/lib/styles/index.css';
-import axios from 'axios';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-interface TestingPdfProps{
-  fileId:string
+interface TestingPdfProps {
+  fileId: string;
 }
 
-function TestingPdf({fileId}:TestingPdfProps) {
-    const [file, setFile] = useState<any>();
-    const fetchPdf = async()=>{
-        const response:any = await axios.get(`http://localhost:5001/api/upload/pdf/${fileId}`);
-        const url = URL.createObjectURL(new Blob([response.data], { type: 'application/pdf' }));
-        setFile(url);
-      }
+function TestingPdf({ fileId }: TestingPdfProps) {
+  const [fileUrl, setFileUrl] = useState<string | undefined>(undefined);
 
-      useEffect(()=>{
-        fetchPdf();
-      },[])
-    const workerUrl = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/3.6.172/pdf.worker.min.js`;
-    return (
-      <div style={{ height: '100vh' }}>
-       {file && 
+  const fetchPdf = async () => {
+    try {
+      const response = await axios.get(`http://localhost:5001/api/upload/pdf/${fileId}`, {
+        responseType: 'blob', // Set the response type to blob
+      });
+      const url = URL.createObjectURL(response.data);
+      setFileUrl(url);
+    } catch (error) {
+      console.error('Error fetching PDF:', error);
+      // Handle error state here
+    }
+  };
+
+  useEffect(() => {
+    fetchPdf();
+  }, []);
+
+  const workerUrl = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/3.6.172/pdf.worker.min.js`;
+
+  return (
+    <div style={{ height: '100vh',width:'100%',overflowY:'scroll' }}>
+      {fileUrl && (
         <Worker workerUrl={workerUrl}>
-        <Viewer fileUrl={file}  plugins={[defaultLayoutPlugin as any]}/>
-      </Worker>
-      }
-      </div>
-    );
-  }
+          <Viewer fileUrl={fileUrl} plugins={[defaultLayoutPlugin as any]} />
+        </Worker>
+      )}
+    </div>
+  );
+}
 
-  export default TestingPdf;
+export default TestingPdf;
