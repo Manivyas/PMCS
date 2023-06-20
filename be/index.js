d8d6a4168c262260eb32a8a123d7326b8d82d1bf
Aedulapuram Manivyas <62017824+Manivyas@users.noreply.github.com>
2023-06-21 01:21:19 +0530

    Update index.js

--- be/index.js
+++ be/index.js
@@ -1,31 +1,25 @@
 const express = require('express');
 const app = express();
 const fileUpload = require('express-fileupload');
-
-
 const authRoute = require('./routes/auth');
 const uploadRoute = require('./routes/upload');
-
-
-const mongoose = require('mongoose')
-
-var cors = require("cors");
+const mongoose = require('mongoose');
+const cors = require('cors');
 const dotenv = require('dotenv');
-const MONGODB_URI = 'mongodb://localhost:27017/mydatabase';
-
-app.use(cors());
 
 dotenv.config();
 
-console.log("Config is", process.env.DB_PATH)
+const { DB_PATH } = process.env;
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () =>
-console.log('connected to db!')
-)
-
+app.use(cors());
 app.use(express.json());
-
 app.use(fileUpload());
+
+// Connect to MongoDB Atlas
+mongoose
+  .connect(DB_PATH, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch((err) => console.error('Failed to connect to MongoDB Atlas:', err));
 
 app.use('/api/user', authRoute);
 app.use('/api/upload', uploadRoute);
